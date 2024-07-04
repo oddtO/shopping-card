0cb727e2473bddd0592bbdbc355201c1cdf0fad7
oddtO <121516833+oddtO@users.noreply.github.com>
2024-07-04 06:04:24 +0300

    install types for sass modules

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
+    /* globals: true,
     environment: "jsdom",
-    setupFiles: "./tests/setup.js",
+    setupFiles: "./tests/setup.js", */
   },
 });
